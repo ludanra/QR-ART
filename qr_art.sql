61b1bed4234d5b8afa0dd6f8f9f9b68cc8d185af
ludanra <luidanramos@gmail.com>
2022-10-27 10:04:50 -0300

    Update qr_art.sql

--- qr_art.sql
+++ qr_art.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-10-2022 a las 19:32:47
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 27-10-2022 a las 14:54:28
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 7.4.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `extra` (
   `cod_extra` int(6) NOT NULL,
-  `cod_prod` int(6) NOT NULL,
   `categ_extra` text NOT NULL,
-  `nombre_extra` text NOT NULL
+  `nombre_extra` text NOT NULL,
+  `precio_extra` decimal(11,0) NOT NULL,
+  `foto_extra` text NOT NULL,
+  `estado_extra` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `extra`
+--
+
+INSERT INTO `extra` (`cod_extra`, `categ_extra`, `nombre_extra`, `precio_extra`, `foto_extra`, `estado_extra`) VALUES
+(1, '1', 'Cheddar', '200', 'chedar.jpg', 0),
+(2, '2', 'Bacon', '200', 'bacon.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -101,7 +111,7 @@ CREATE TABLE `productos` (
   `nombre_prod` text NOT NULL,
   `precio_prod` decimal(10,0) NOT NULL,
   `detalle_prod` text NOT NULL,
-  `foto_prod` int(11) NOT NULL,
+  `foto_prod` text NOT NULL,
   `prod_disponible` tinyint(1) NOT NULL,
   `requiere_cocina` tinyint(1) NOT NULL,
   `est_baja_prod` tinyint(1) NOT NULL,
@@ -113,8 +123,10 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`cod_prod`, `categoria_prod`, `nombre_prod`, `precio_prod`, `detalle_prod`, `foto_prod`, `prod_disponible`, `requiere_cocina`, `est_baja_prod`, `categ_extra`) VALUES
-(2, '', 'hamburguesa', '500', '', 0, 0, 0, 0, ''),
-(3, '', 'hamburguesa', '500', '', 0, 0, 0, 0, '');
+(4, '4', 'Prueba Foto', '540', 'Prueba de foto', 'ok.jpg', 0, 0, 0, '3'),
+(5, '1', 'prueba foto 2', '658', 'prueba foto 2', 'ok.jpg', 0, 0, 0, '1'),
+(7, '1', 'Hamburguesa 1', '980', 'Hamburguesa Clásica con lechuga, tomate y cheddar', 'Hamburguesa1.jpg', 0, 0, 0, '1'),
+(8, '3', 'Producto de prueba 24-10', '700', 'Producto de prueba', 'Hamburguesa2.jpg', 0, 0, 0, '3');
 
 -- --------------------------------------------------------
 
@@ -139,16 +151,15 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`usuario`, `contrasena`, `cod_perfil`, `nombre_usu`, `apellido_usu`, `est_baja_usu`, `email_usu`, `id_usuario`, `token`) VALUES
-('Mia19', '81dc9bdb52d04dc20036dbd8313ed055', 2, 'Mia', 'fleita', 'ACTIVO', 'miafleita12@gmail.com', 47, ''),
-('antoo', 'b59c67bf196a4758191e42f76670ceba', 1, 'antonella', 'alfonso', 'ACTIVO', 'antoo@gmail.com', 56, ''),
-('Marting', '81dc9bdb52d04dc20036dbd8313ed055', 1, 'Martin', 'fleita', 'INACTIVO', 'Martingerardo@gmail.com', 68, ''),
-('Boca', '81dc9bdb52d04dc20036dbd8313ed055', 1, 'Boca', 'Juniors', 'INACTIVO', 'bocajuniors@gmail.com', 70, ''),
-('nicole', '81dc9bdb52d04dc20036dbd8313ed055', 2, 'Nicole', 'fleita', 'ACTIVO', 'nicole@gmail.com', 71, ''),
-('tana', '81dc9bdb52d04dc20036dbd8313ed055', 2, 'Nicole', 'fleita', 'ACTIVO', 'tana@gmail.com', 72, ''),
-('pepe', '81dc9bdb52d04dc20036dbd8313ed055', 2, 'Nicole', 'fleita', 'ACTIVO', 'pepe@gmail.com', 73, ''),
-('tina', '81dc9bdb52d04dc20036dbd8313ed055', 3, 'aaaa', 'asss', 'ACTIVO', 'ssdfgt@sllslsl', 74, ''),
-('', 'd41d8cd98f00b204e9800998ecf8427e', 0, '', '', '', '', 75, ''),
-('hola', '202cb962ac59075b964b07152d234b70', 1, 'hohoh', 'aaamanm', 'ACTIVO', 'aaam@jsjsjs', 76, '');
+('Mia19', 'b59c67bf196a4758191e42f76670ceba', 2, 'Mia', 'fleita', '', 'miafleita12@gmail.com', 47, '633ed1bd2c1a2'),
+('antoo', '81dc9bdb52d04dc20036dbd8313ed055', 1, 'antonella', 'alfonso', 'ACTIVO', 'antoo@gmail.com', 56, '634577ce44021'),
+('Marting', 'e10adc3949ba59abbe56e057f20f883e', 1, 'Martin', 'fleita', 'INACTIVO', 'Martingerardo@gmail.com', 68, '63457003e3c65'),
+('Boca', 'b84bd99d065f6aab6fcd590ec1b5fe16', 3, 'Boca', 'Juniors', 'INACTIVO', 'bocajuniors@gmail.com', 70, ''),
+('nicole', 'e10adc3949ba59abbe56e057f20f883e', 2, 'Nicole', 'fleita', 'ACTIVO', 'nicole@gmail.com', 71, ''),
+('tana', 'e10adc3949ba59abbe56e057f20f883e', 2, 'Nicole', 'fleita', 'ACTIVO', 'tana@gmail.com', 72, ''),
+('pepe', 'e10adc3949ba59abbe56e057f20f883e', 2, 'Nicole', 'fleita', 'ACTIVO', 'pepe@gmail.com', 73, ''),
+('tina', '4124bc0a9335c27f086f24ba207a4912', 3, 'aaaa', 'asss', 'ACTIVO', 'ssdfgt@sllslsl', 74, ''),
+('ludanra', '81dc9bdb52d04dc20036dbd8313ed055', 1, 'Luis Daniel', 'Ramos', 'ACTIVO', 'luidanramos@gmail.com', 75, '6356fecbc4d25');
 
 --
 -- Índices para tablas volcadas
@@ -200,6 +211,12 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `extra`
+--
+ALTER TABLE `extra`
+  MODIFY `cod_extra` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de la tabla `perfiles`
 --
 ALTER TABLE `perfiles`
@@ -209,23 +226,17 @@ ALTER TABLE `perfiles`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `cod_prod` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `cod_prod` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `extra`
---
-ALTER TABLE `extra`
-  ADD CONSTRAINT `extra_ibfk_1` FOREIGN KEY (`cod_extra`) REFERENCES `pedidos` (`cod_extra`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `pedidos`
